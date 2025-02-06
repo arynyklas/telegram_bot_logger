6bda7cb724c9f29493521f1acfbc36cfa1b7616e
Aryn Yklas <arynyklas@gmail.com>
2025-02-06 23:36:16 +0500

    v0.0.4.post0

--- telegram_bot_logger/__init__.py
+++ telegram_bot_logger/__init__.py
@@ -2,4 +2,4 @@ from . import handlers, formatters, api_server, utils, exceptions  # noqa
 from .handlers import TelegramMessageHandler  # noqa
 
 
-__version__ = "0.0.4"
+__version__ = "0.0.4.post0"
